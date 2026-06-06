f35aae2aca969e3194d5cf347bcbfe82c9fb17c1
Padmanabh Sundaram <164679021+Padmanabh31@users.noreply.github.com>
2026-06-06 09:35:50 +0530

    Implement Valid Anagram solution in C++

---
+++ ValidAnagram.cpp
@@ -0,0 +1,29 @@
+
+
+/*
+Topic: String
+Problem :  Valid Anagram
+Problem no. :- 242
+Link: https://leetcode.com/problems/valid-anagram/description/
+Difficulty :- easy
+*/
+
+
+class Solution {
+public:
+    bool isAnagram(string s , string t ) {
+        int table [256] ={0};
+        for (int i=0 ; i <s.size() ; i++){
+            table[s[i]]++ ;
+        }
+        for (int i=0 ; i < t.size() ; i++){
+            table[t[i]]-- ;
+        }
+        for (int i=0 ; i <256 ; i++){
+            if(table[i]!=0) {
+            return false ;
+            }
+        }
+        return true ; 
+    }
+};
