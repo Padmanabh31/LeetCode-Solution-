3f3799aa5ce71af8718f57ae8b0cc4024f2e481c
Padmanabh Sundaram <164679021+Padmanabh31@users.noreply.github.com>
2026-06-06 10:25:56 +0530

    Add reverseOnlyLetters function in Solution class
    Implement function to reverse only letters in a string.

---
+++ ReverseOnlyLetters.cpp
@@ -0,0 +1,31 @@
+
+
+/*
+Topic: String
+Problem :  Reverse Only Letters
+Problem no. :- 917
+Link: https://leetcode.com/problems/reverse-only-letters/description/
+Difficulty :- easy
+*/
+
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int i=0 ;
+        int j= s.size()-1 ; 
+        while (i <=j) {
+            if (isalpha(s[i]) && isalpha(s[j])){
+                swap(s[i] , s[j]) ; 
+                i++ ;
+                j-- ;
+            }
+            else if (!isalpha(s[i])){
+                i++ ;
+            }
+            else {
+                j-- ;
+            }
+        }
+        return s ; 
+    }
+};
